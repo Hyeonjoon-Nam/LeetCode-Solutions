4ad91ff8746daa2063ca51d478f29921ceb0e3a1
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-24 14:10:23 -0800

    Time: 103 ms (5.06%), Space: 69.8 MB (5.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,28 +3,61 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // ================================
         // Approach 1: Sorting + unordered_map
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // - Time  Complexity: O(N * K log K)
+        //   (We iterate N strings, and sorting each string takes O(K log K) where K is max string length)
+        // - Space Complexity: O(N * K)
+        //   (To store all strings in the map)
+        // - Pros: Easy to implement
+        // - Cons: Sorting cost (K log K)
+        // ================================
+        // unordered_map<string, vector<string>> count;
+
+        // for (string str : strs)
+        // {
+        //     string sorted = str;
+        //     sort(sorted.begin(), sorted.end());
+
+        //     count[sorted].push_back(str);
+        // }
+        // vector<vector<string>> ans;
+        // for (auto& it : count)
+        // {
+        //     ans.push_back(it.second);
+        // }
+
+        // return ans;
+        
+        // ================================
+        // Approach 2: Counting(Not sorting) + unordered_map
+        // - Time  Complexity: O(N * K log K)
+        //   (We iterate N strings, and sorting each string takes O(K log K) where K is max string length)
+        // - Space Complexity: O(N * K)
+        //   (To store all strings in the map)
+        // - Pros: Easy to implement
+        // - Cons: Sorting cost (K log K)
         // ================================
         unordered_map<string, vector<string>> count;
 
         for (string str : strs)
         {
-            string sorted = str;
-            sort(sorted.begin(), sorted.end());
+            int* temp = new int[26];
 
-            count[sorted].push_back(str);
+            for (char c : str)
+            {
+                temp[c-'a']++;
+            }
+            string key = "";
+            for (int i = 0; i < 26; i++)
+            {
+                key += to_string(temp[i]);
+            }
+            count[key].push_back(str);
         }
         vector<vector<string>> ans;
-        for (auto& it : count)
+        for (auto it : count)
         {
             ans.push_back(it.second);
         }
-
         return ans;
     }
 };
