a079fe95422426982e878e2493663596060d8637
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-24 14:15:31 -0800

    Time: 14 ms (74.42%), Space: 25 MB (64.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // ================================
         // Approach 1: Sorting + unordered_map
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // - Time  Complexity: O(N * K log K)
+        //   (We iterate N strings, and sorting each string takes O(K log K) where K is max string length)
+        // - Space Complexity: O(N * K)
+        //   (To store all strings in map)
+        // - Pros: Easy to implement
+        // - Cons: Sorting cost (K log K)
         // ================================
         unordered_map<string, vector<string>> count;
 
@@ -26,5 +26,40 @@ public:
         }
 
         return ans;
+        
+        // ================================
+        // Approach 2: Counting(Not sorting) + unordered_map
+        // - Time  Complexity: O(N * K)
+        //   (We iterate N strings, and we count chars in O(K).)
+        // - Space Complexity: O(N * K)
+        //   (To store all strings in map)
+        // - Pros: Faster than sorting when K is large.
+        // - Cons: Key generation overhead
+        // ================================
+        // unordered_map<string, vector<string>> count;
+
+        // for (string str : strs)
+        // {
+        //     vector<int> freq(26, 0);
+        //     for (char c : str)
+        //     {
+        //         freq[c-'a']++;
+        //     }
+
+        //     string key = "";
+        //     for (int i = 0; i < 26; ++i)
+        //     {
+        //         key += freq[i];
+        //     }
+
+        //     count[key].push_back(str);
+        // }
+        
+        // vector<vector<string>> ans;
+        // for (auto& it : count)
+        // {
+        //     ans.push_back(it.second);
+        // }
+        // return ans;
     }
 };
