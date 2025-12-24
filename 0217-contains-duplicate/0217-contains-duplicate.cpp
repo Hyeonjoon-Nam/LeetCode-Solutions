cc5eed0b9b425e3e1619ea7efff9e2674a7127f3
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-24 10:51:32 -0800

    Time: 17 ms (95.23%), Space: 70.9 MB (95.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,42 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // ================================
+        // Approach 1: Using set.
+        // ================================
+        // set<int> seen;
 
+        // for (int x : nums)
+        // {
+        //     if (seen.find(x) != seen.end())
+        //         return true;
+
+        //     seen.insert(x);
+        // }
+        // return false;
+
+        // ================================
+        // Approach 2: Using unordered_set
+        // ================================
+        // unordered_set<int> seen;
+
+        // for (int x : nums)
+        // {
+        //     if (seen.find(x) != seen.end())
+        //         return true;
+            
+        //     seen.insert(x);
+        // }
+        // return false;
+        
+        // ================================
+        // Approach 3: Sorting
+        // ================================
+        sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 1; ++i)
         {
-            if (nums[i] == nums[i+1])
-                return true;
+            if (nums[i] == nums[i+1]) return true;
         }
-
         return false;
     }
 };
