4ba7f345d513b273c4aeb911ab8c57237f8f65c6
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-24 11:05:48 -0800

    Time: 30 ms (77.3%), Space: 71 MB (88.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,6 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         // ================================
         // Approach 1: Using set.
+        // - Time  Complexity: O(N log N)
+        //   (Insert/Find takes O(log N), we do this N times.)
+        // - Space Complexity: O(N)
+        // - Pros: Data are always sorted (ordered)
+        // - Cons: slower than unordered_set
+        //         no reason to sort to solve this problem
         // ================================
         // set<int> seen;
 
@@ -17,6 +23,10 @@ public:
 
         // ================================
         // Approach 2: Using unordered_set
+        // - Time  Complexity: O(N)
+        // - Space Complexity: O(N)
+        // - Pros: Best time complexity
+        // - Cons: Space overhead
         // ================================
         // unordered_set<int> seen;
 
@@ -31,6 +41,12 @@ public:
         
         // ================================
         // Approach 3: Sorting
+        // - Time  Complexity: O(N log N)
+        //   (std::sort uses Introsort which is O(N log N))
+        // - Space Complexity: O(1)
+        //   (No extra data structure used.)
+        // - Pros: Space efficient
+        // - Cons: Changes the original order of the number
         // ================================
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 1; ++i)
