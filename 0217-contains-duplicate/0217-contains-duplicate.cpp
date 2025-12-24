24fa970503f5eeb5a80ff6a8bc9a0225b03a3ca4
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-24 11:07:25 -0800

    Time: 43 ms (74.09%), Space: 90.8 MB (44.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // ================================
         // Approach 1: Using set.
-        // - Time  Complexity: O(N log N)
-        //   (Insert/Find takes O(log N), we do this N times.)
-        // - Space Complexity: O(N)
-        // - Pros: Data are always sorted (ordered)
-        // - Cons: slower than unordered_set
-        //         no reason to sort to solve this problem
-        // ================================
         // set<int> seen;
 
         // for (int x : nums)
@@ -19,40 +11,20 @@ public:
 
         //     seen.insert(x);
         // }
+
         // return false;
 
-        // ================================
         // Approach 2: Using unordered_set
-        // - Time  Complexity: O(N)
-        // - Space Complexity: O(N)
-        // - Pros: Best time complexity
-        // - Cons: Space overhead
-        // ================================
-        // unordered_set<int> seen;
+        unordered_set<int> seen;
 
-        // for (int x : nums)
-        // {
-        //     if (seen.find(x) != seen.end())
-        //         return true;
-            
-        //     seen.insert(x);
-        // }
-        // return false;
-        
-        // ================================
-        // Approach 3: Sorting
-        // - Time  Complexity: O(N log N)
-        //   (std::sort uses Introsort which is O(N log N))
-        // - Space Complexity: O(1)
-        //   (No extra data structure used.)
-        // - Pros: Space efficient
-        // - Cons: Changes the original order of the number
-        // ================================
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 1; ++i)
+        for (int x : nums)
         {
-            if (nums[i] == nums[i+1]) return true;
+            if (seen.find(x) != seen.end())
+                return true;
+            
+            seen.insert(x);
         }
+        
         return false;
     }
 };
