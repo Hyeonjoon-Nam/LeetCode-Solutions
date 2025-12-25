3b2c56ea8925ef0a6ad4f43a786d3b87d9d33154
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-25 09:49:20 -0800

    Time: 3 ms (67.73%), Space: 14.8 MB (53.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,6 +3,28 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // ================================
         // Approach 1: brute force
+        // - Time  Complexity: O(N²)
+        //   (N * N times)
+        // - Space Complexity: O(1)
+        //   (No extra space used)
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // int size = nums.size();
+        // for (int i = 0; i < nums.size() - 1; ++i)
+        // {
+        //     for (int j = i + 1; j < nums.size(); ++j)
+        //     {
+        //         if (i == j) continue;
+
+        //         if (nums[i] + nums[j] == target) return { i, j };
+        //     }
+        // }
+        // return {};
+
+
+        // ================================
+        // Approach 2: hash map
         // - Time  Complexity: O()
         //   ()
         // - Space Complexity: O()
@@ -10,41 +32,26 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
-        int size = nums.size();
-        for (int i = 0; i < nums.size() - 1; ++i)
+        vector<pair<int, int>> nice;
+        for (int i = 0; i < nums.size(); ++i)
         {
-            for (int j = i + 1; j < nums.size(); ++j)
-            {
-                if (i == j) continue;
+            nice.push_back({ nums[i], i });
+        }
+        sort(nice.begin(), nice.end());
 
-                if (nums[i] + nums[j] == target) return { i, j };
+        int p0 = 0;
+        int p1 = nums.size() - 1;
+
+        while (p0 < p1)
+        {
+            int sum = nice[p0].first + nice[p1].first;
+            if (sum == target)
+            {
+                return {nice[p0].second, nice[p1].second};
             }
+            else if (sum > target) p1--;
+            else p0++;
         }
         return {};
-
-        
-        // ================================
-        // Approach 2: std::vector::find
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
-        // ================================
-
-        for (int i = 0; i < nums.size() - 1; ++i)
-        {
-            int cur = nums[i];
-
-            auto it = find(nums.begin(), nums.end(), target - cur);
-            if (it != nums.end())
-            {
-                int dist = it - nums.begin();
-                if (dist == 0) continue;
-
-                return {i, dist};
-            }
-        }
     }
 };
