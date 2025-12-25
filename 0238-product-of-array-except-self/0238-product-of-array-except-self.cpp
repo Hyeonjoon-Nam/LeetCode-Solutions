b37d0f8f5e113a177954225c7a764e622ab9c2b5
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-25 11:33:24 -0800

    Time: 3 ms (38.99%), Space: 40.2 MB (78.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -10,7 +10,7 @@ public:
         // - Pros: Easy to code
         // - Cons: Time limit exceeds
         // ================================
-        // vector<int> sol;
+        // vector<int> ans;
         // for (int i = 0; i < nums.size(); i++)
         // {
         //     int mul = 1;
@@ -19,7 +19,7 @@ public:
         //         if (i == j) continue;
         //         mul *= nums[j];
         //     }
-        //     sol.push_back(mul);
+        //     ans.push_back(mul);
         // }
         // return sol;
 
@@ -33,42 +33,68 @@ public:
         // - Pros: Better than O(N²)
         // - Cons: Little bit harder to implement
         // ================================
-        vector<int> leftToRight;
-        leftToRight.reserve(nums.size());
-        int mul = 1;
-        for (int i = 0; i < nums.size(); i++)
+        // vector<int> leftToRight;
+        // leftToRight.reserve(nums.size());
+        // int mul = 1;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     mul *= nums[i];
+        //     leftToRight.emplace_back(mul);
+        // }
+
+        // vector<int> rightToLeft;
+        // rightToLeft.reserve(nums.size());
+        // mul = 1;
+        // for (int i = nums.size() - 1; i >= 0; i--)
+        // {
+        //     mul *= nums[i];
+        //     rightToLeft.emplace_back(mul);
+        // }
+        // reverse(rightToLeft.begin(), rightToLeft.end());
+
+        // vector<int> ans;
+        // ans.reserve(nums.size());
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     if (i == 0)
+        //     {
+        //         ans.emplace_back(rightToLeft[i+1]);
+        //     }
+        //     else if (i == nums.size() - 1)
+        //     {
+        //         ans.emplace_back(leftToRight[i - 1]);
+        //     }
+        //     else
+        //     {
+        //         ans.emplace_back(leftToRight[i - 1] * rightToLeft[i + 1]);
+        //     }
+        // }
+        // return ans;
+
+        
+        // ================================
+        // Approach 3: 
+        // - Time  Complexity: O(N)
+        //   (Traverse numbers N times)
+        // - Space Complexity: O(1)
+        //   (No extra space used except for the vector for ouput)
+        // - Pros: Better than O(N²), space efficient
+        // - Cons: Hard to think of it
+        // ================================
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        for (int i = 1; i < nums.size(); ++i)
         {
-            mul *= nums[i];
-            leftToRight.emplace_back(mul);
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
 
-        vector<int> rightToLeft;
-        rightToLeft.reserve(nums.size());
-        mul = 1;
-        for (int i = nums.size() - 1; i >= 0; i--)
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--)
         {
-            mul *= nums[i];
-            rightToLeft.emplace_back(mul);
+            ans[i] *= right;
+            right *= nums[i];
         }
-        reverse(rightToLeft.begin(), rightToLeft.end());
 
-        vector<int> sol;
-        sol.reserve(nums.size());
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (i == 0)
-            {
-                sol.emplace_back(rightToLeft[i+1]);
-            }
-            else if (i == nums.size() - 1)
-            {
-                sol.emplace_back(leftToRight[i - 1]);
-            }
-            else
-            {
-                sol.emplace_back(leftToRight[i - 1] * rightToLeft[i + 1]);
-            }
-        }
-        return sol;
+        return ans;
     }
 };
