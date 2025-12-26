7d44f86ee95e48c97c1aec4a3750a5dc448f7a0c
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-26 11:28:09 -0800

    Time: 3 ms (55.25%), Space: 23.1 MB (55.54%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,6 +3,68 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         // ================================
         // Approach 1: Hash map
+        // - Time  Complexity: O(N)
+        //   (Use multiple for-loop, but the total number of call is equals to O(N))
+        // - Space Complexity: O(N)
+        //   (To store numbers to unordered_set)
+        // - Pros: Intuitive
+        // - Cons: 
+        // ================================
+        // int size = 9;
+        
+        // // Check each 3 * 3 cell
+        // for (int i = 0; i < size; i += 3)
+        // {
+        //     for (int j = 0; j < size; j += 3)
+        //     {
+        //         hash.clear();
+        //         for (int row = i; row < i + 3; row++)
+        //         {
+        //             for (int col = j; col < j + 3; col++)
+        //             {
+        //                 if (board[row][col] == '.') continue;
+        //                 if (hash.find(board[row][col]) != hash.end())
+        //                     return false;
+                        
+        //                 hash.insert(board[row][col]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // // Check each col
+        // for (int row = 0; row < size; ++row)
+        // {
+        //     hash.clear();
+        //     for (int col = 0; col < size; ++col)
+        //     {
+        //         if (board[row][col] == '.') continue;
+        //         if (hash.find(board[row][col]) != hash.end())
+        //             return false;
+                
+        //         hash.insert(board[row][col]);
+        //     }
+        // }
+
+        // // Check each row
+        // for (int col = 0; col < size; ++col)
+        // {
+        //     hash.clear();
+        //     for (int row = 0; row < size; ++row)
+        //     {
+        //         if (board[row][col] == '.') continue;
+        //         if (hash.find(board[row][col]) != hash.end())
+        //             return false;
+                
+        //         hash.insert(board[row][col]);
+        //     }
+        // }
+
+        // return true;
+
+        
+        // ================================
+        // Approach 2: Single pass with fixed size array
         // - Time  Complexity: O()
         //   ()
         // - Space Complexity: O()
@@ -10,57 +72,29 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
-        unordered_set<int> hash;
         int size = 9;
-        
-        // Check each 3 * 3 cell
-        for (int i = 0; i < size; i += 3)
-        {
-            for (int j = 0; j < size; j += 3)
-            {
-                hash.clear();
-                for (int row = i; row < i + 3; row++)
-                {
-                    for (int col = j; col < j + 3; col++)
-                    {
-                        if (board[row][col] == '.') continue;
-                        if (hash.find(board[row][col]) != hash.end())
-                            return false;
-                        
-                        hash.insert(board[row][col]);
-                    }
-                }
-            }
-        }
+        vector<vector<bool>>  rows(size, vector<bool>(size, 0));
+        vector<vector<bool>>  cols(size, vector<bool>(size, 0));
+        vector<vector<bool>> boxes(size, vector<bool>(size, 0));
 
-        // Check each col
-        for (int row = 0; row < size; ++row)
+        for (int row = 0; row < size; row++)
         {
-            hash.clear();
-            for (int col = 0; col < size; ++col)
+            for (int col = 0; col < size; col++)
             {
                 if (board[row][col] == '.') continue;
-                if (hash.find(board[row][col]) != hash.end())
-                    return false;
-                
-                hash.insert(board[row][col]);
+                int pos = board[row][col] - '1';
+
+                if (rows[row][pos] == true) return false;
+                rows[row][pos] = true;
+
+                if (cols[col][pos] == true) return false;
+                cols[col][pos] = true;
+
+                int index = (row / 3) * 3 + col / 3;
+                if (boxes[index][pos] == true) return false;
+                boxes[index][pos] = true;
             }
         }
-
-        // Check each row
-        for (int col = 0; col < size; ++col)
-        {
-            hash.clear();
-            for (int row = 0; row < size; ++row)
-            {
-                if (board[row][col] == '.') continue;
-                if (hash.find(board[row][col]) != hash.end())
-                    return false;
-                
-                hash.insert(board[row][col]);
-            }
-        }
-
         return true;
     }
 };
