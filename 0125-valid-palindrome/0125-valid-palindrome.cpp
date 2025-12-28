f27216d70e3fd7b12634e342e3c814bf45d1fb8c
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-27 16:42:18 -0800

    Time: 1 ms (46.67%), Space: 10 MB (39.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        // ================================
+        // Approach 1: Two pointers
+        // - Time  Complexity: O(N)
+        //   (Iterate all characters)
+        // - Space Complexity: O(1)
+        //   (No extra space used)
+        // - Pros: Intuitive
+        // - Cons: 
+        // ================================
         int n = s.length();
         if (n == 0) return true;
 
@@ -9,13 +18,26 @@ public:
 
         while (p0 < p1)
         {
-            if (!isalpha(s[p0]) && !isdigit(s[p0])) { p0++; continue; }
-            if (!isalpha(s[p1]) && !isdigit(s[p1])) { p1--; continue; }
+            // if (!isalpha(s[p0]) && !isdigit(s[p0])) { p0++; continue; }
+            // if (!isalpha(s[p1]) && !isdigit(s[p1])) { p1--; continue; }
+            if (!isalnum(s[p0])) { p0++; continue; }
+            if (!isalnum(s[p1])) { p1--; continue; }
             
             if (tolower(s[p0]) != tolower(s[p1])) return false;
             ++p0;
             --p1;
         }
         return true;
+
+        
+        // ================================
+        // Approach 1: Two pointers
+        // - Time  Complexity: O(N)
+        //   (Iterate all characters)
+        // - Space Complexity: O(1)
+        //   (No extra space used)
+        // - Pros: Intuitive
+        // - Cons: 
+        // ================================
     }
 };
