95f41e801b7a4e9398efe45b25888c035ea1782a
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2025-12-30 11:42:11 -0800

    Time: 8 ms (55.19%), Space: 11.8 MB (63.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // Approach 1: Sliding window
+        // - Time  Complexity: O(N)
+        //   (Iterate elements once, insert/find functions of unordered_map takes O(1))
+        // - Space Complexity: O(min(m, n))
+        //   (m is the size of character set, n is the length of the string)
+        // - Pros: Optimal time and space
+        // - Cons: Hash map overhead
         // ================================
         unordered_map<char, int> hash;
         int left = 0;
