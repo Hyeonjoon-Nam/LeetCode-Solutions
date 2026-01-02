311a1da932e634b925ff9bc66cbbee9e1c7c6ac2
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-02 15:27:13 -0800

    Time: 35 ms (26.83%), Space: 141.5 MB (21.63%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // ================================
+        // Approach 1: Brute force
+        // - Time  Complexity: O(N²)
+        //   (Iterate elements using two for-loops)
+        // - Space Complexity: O(1)
+        //   (Constant space used except for a vector for the output)
+        // - Pros: Intuitive, easy to code
+        // - Cons: Time limit exceeds
+        // ================================
+        // size_t n = nums.size();
+        // vector<int> ans;
+
+        // for (int i = 0; i <= n - k; i++)
+        // {
+        //     int max_num = INT_MIN;
+        //     for (int j = i; j < i + k; j++)
+        //     {
+        //         max_num = max(max_num, nums[j]);
+        //     }
+        //     ans.push_back(max_num);
+        // }
+        // return ans;
+
+
+        // ================================
+        // Approach 2: Sliding window
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        vector<int> ans;
+        size_t n = nums.size();
+        deque<pair<int, int>> myDeque;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+
+            while (!myDeque.empty() && myDeque.back().first < num)
+                myDeque.pop_back();
+
+            myDeque.push_back({num, i});
+
+            if (myDeque.front().second <= i - k)
+                myDeque.pop_front();
+
+            if (i >= k - 1)
+                ans.push_back(myDeque.front().first);
+        }
+        return ans;
+    }
+};
