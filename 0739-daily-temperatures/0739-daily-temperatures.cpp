7d078e821333798986cbfa8b6e3e7b64c6a448bd
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-05 16:06:48 -0800

    Time: 23 ms (54.88%), Space: 107.3 MB (55.54%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // ================================
+        // Approach 1: Brute force
+        // - Time  Complexity: O(N²)
+        //   (Iterate two for-loops)
+        // - Space Complexity: O(1)
+        //   ()
+        // - Pros: Easy to implement
+        // - Cons: Time limit exceeds
+        // ================================
+        // int n = temperatures.size();
+        // vector<int> ans;
+
+        // for (int i = 0; i < n - 1; i++)
+        // {
+        //     int cur = temperatures[i];
+        //     int future = i;
+        //     for (int j = i + 1; j < n; j++)
+        //     {
+        //         int temp = temperatures[j];
+
+        //         if (temp > cur)
+        //         {
+        //             future = j;
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back(future - i);
+        // }
+        // ans.push_back(0);
+        // return ans;
+
+        
+        // ================================
+        // Approach 2: Stack
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        stack<int> stk;
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = temperatures[i];
+
+            while (!stk.empty() && temp > temperatures[stk.top()])
+            {
+                int prev = stk.top(); stk.pop();
+                ans[prev] = i - prev;
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
