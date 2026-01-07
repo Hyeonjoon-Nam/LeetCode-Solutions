93fdb7f64ae579dbeba500294fa3f67f8b826509
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-07 10:04:26 -0800

    Time: 0 ms (100%), Space: 13.3 MB (75.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,34 +4,50 @@ public:
         // ================================
         // Approach 1: Linear
         // - Time  Complexity: O(m*n)
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
+        //   (Lineary iterate all the elements)
+        // - Space Complexity: O(1)
+        //   (No extra space used)
+        // - Pros: Intuitive
+        // - Cons: Doesn't meet the requirement O(log(m*n))
+        // ================================
+        // int row = matrix.size();
+        // int col = matrix[0].size();
+
+        // for (int r = 0; r < row; r++)
+        // {
+        //     for (int c = 0; c < col; c++)
+        //     {
+        //         int num = matrix[r][c];
+        //         if (num == target) return true;
+        //         else if (num < target) continue;
+        //         else return false;
+        //     }
+        // }
+        // return false;
+
+        // ================================
+        // Approach 2: Binary search
+        // - Time  Complexity: O(log(m*n))
+        //   (Treat the 2D matrix as a sorted 1D array)
+        // - Space Complexity: O(1)
+        //   (No extra space used)
+        // - Pros: Cleaner logic, single binary search pass
         // - Cons: 
         // ================================
-        int row = matrix.size();
-        int col = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0;
+        int right = m * n - 1;
 
-        for (int r = 0; r < row; r++)
+        while (left <= right)
         {
-            for (int c = 0; c < col; c++)
-            {
-                int num = matrix[r][c];
-                if (num == target) return true;
-                else if (num < target) continue;
-                else return false;
-            }
+            int mid = left + (right - left) / 2;
+            int num = matrix[mid/n][mid%n];
+
+            if (num == target) return true;
+            else if (num < target) left = mid + 1;
+            else right = mid - 1;
         }
         return false;
-        // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
-        // ================================
     }
 };
