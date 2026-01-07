f7121476c086c92821ee56adf2dd4312e1f1e65a
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-07 10:38:12 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // ================================
+        // Approach 1: Binary search
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        int n = piles.size();
+        int left = 1;
+        int right = 0;
+        int ans = 0;
+        for (int x : piles) right = max(right, x);
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int time = 0;
+            for (int x : piles)
+            {
+                time += (x + mid - 1) / mid;
+            }
+            if (time <= h)
+            {
+                ans = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
