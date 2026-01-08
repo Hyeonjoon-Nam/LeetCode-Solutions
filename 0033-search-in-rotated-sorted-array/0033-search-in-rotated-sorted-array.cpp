1e448b56e86cef496429c4a3bfc4a0b2311a4bac
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-08 10:53:14 -0800

    Time: 0 ms (100%), Space: 15.1 MB (94.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,48 +10,87 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
-        int n = nums.size();
-        int left = 0, right = n - 1;
-        int mid = 0;
+        // int n = nums.size();
+        // if (n == 0) return -1;
+        // if (n == 1) return nums[0] == target ? 0 : -1;
+        // int pivot = -1;
+
+        // if (nums[0] > nums[n - 1])
+        // {
+        //     int left = 0, right = n - 1;
+
+        //     while (left <= right)
+        //     {
+        //         int mid = left + (right - left) / 2;
+
+        //         if (mid < n - 1 && nums[mid] > nums[mid + 1])
+        //         {
+        //             pivot = mid;
+        //             break;
+        //         }
+        //         if (mid > 0 && nums[mid - 1] > nums[mid])
+        //         {
+        //             pivot = mid - 1;
+        //             break;
+        //         }
+
+        //         if (nums[mid] >= nums[0]) left = mid + 1;
+        //         else right = mid - 1;
+        //     }
+        // }
+        // else
+        //     pivot = n - 1;
         
-        if (n == 1) return nums[0] == target ? 0 : -1;
-        if (!(nums[right] > nums[0]))
-        {
-            while (left <= right)
-            {
-                mid = left + (right - left) / 2;
-
-                if (nums[mid] > nums[mid+1]) break;
-                if (nums[mid-1] < nums[mid]) { mid--; break;}
-
-                if (nums[mid] > nums[0]) left = mid + 1;
-                else right = mid - 1;
-            }
-        }
-
-        if (target > nums[mid]) return -1;
-        if (target < nums[mid + 1]) return -1;
-
-        if (target >= nums[0])
-        {
-            left = 0;
-            right = mid;
-        }
-        else
-        {
-            left = mid + 1;
-            right = n - 1;
-        }
+        // int left, right;
+        // if (target >= nums[0])
+        // {
+        //     left = 0;
+        //     right = pivot;
+        // }
+        // else
+        // {
+        //     left = pivot + 1;
+        //     right = n - 1;
+        // }
         
+        // while (left <= right)
+        // {
+        //     int mid = left + (right - left) / 2;
+
+        //     if (nums[mid] == target) return mid;
+        //     else if (nums[mid] < target) left = mid + 1;
+        //     else right = mid - 1;
+        // }
+        
+        // return -1;
+        
+        // ================================
+        // Approach 2: One-Pass Binary Search
+        // - Time  Complexity: O(log N)
+        // - Space Complexity: O(1)
+        // ================================
+        int left = 0, right = nums.size() - 1;
+
         while (left <= right)
         {
             int mid = left + (right - left) / 2;
-
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
+
+            if (nums[left] <= nums[mid])
+            {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
-        
         return -1;
     }
 };
