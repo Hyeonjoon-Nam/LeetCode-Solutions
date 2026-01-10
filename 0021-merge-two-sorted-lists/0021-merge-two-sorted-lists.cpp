263bf01b237ec12313b7549abefd8d414834b121
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-10 14:49:18 -0800

    Time: 7 ms (0.96%), Space: 19.7 MB (9.46%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // ================================
+        // Approach 1: Copy and sort
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        vector<int> sorted;
+        ListNode* temp = list1;
+        while (temp != nullptr) {
+            sorted.push_back(temp->val);
+            temp = temp->next;
+        }
+        temp = list2;
+        while (temp != nullptr) {
+            sorted.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(sorted.begin(), sorted.end());
+        ListNode* head = new ListNode(0);
+        ListNode* ans = head;
+        for (int x : sorted) {
+            head ->next = new ListNode(x);
+            head = head->next;
+        }
+        return ans->next;
+
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+    }
+};
