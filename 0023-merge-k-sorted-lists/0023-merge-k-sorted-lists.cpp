c773913ca12c15cede1a6f1be385f72fa0f41d0e
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-14 10:10:30 -0800

    Time: 3 ms (65.42%), Space: 18.9 MB (24.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -12,40 +12,40 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         // ================================
-        // Approach 1: 
+        // Approach 1: Collect, sort, and rebuild
         // - Time  Complexity: O(N log N)
-        //   ()
-        // - Space Complexity: O(N log N)
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        //   (N is the total number of nodes, sorting takes O(N log N))
+        // - Space Complexity: O(N)
+        //   (Storing nodes to a vector)
+        // - Pros: Quite fast. std::sort is higly optimized and vector has excellent cache locality.
+        // - Cons: High memory usage. Creates new nodes (heap overhead)
         // ================================
-        ListNode dummy(0);
-        ListNode* head = &dummy;
-        vector<int> temp;
-        for (ListNode* l : lists) {
-            while (l != nullptr) {
-                temp.push_back(l->val);
-                l = l->next;
-            }
-        }
-        sort(temp.begin(), temp.end());
+        // ListNode dummy(0);
+        // ListNode* head = &dummy;
+        // vector<int> temp;
+        // for (ListNode* l : lists) {
+        //     while (l != nullptr) {
+        //         temp.push_back(l->val);
+        //         l = l->next;
+        //     }
+        // }
+        // sort(temp.begin(), temp.end());
 
-        for (int x : temp) {
-            head->next = new ListNode(x);
-            head = head->next;
-        }
+        // for (int x : temp) {
+        //     head->next = new ListNode(x);
+        //     head = head->next;
+        // }
 
-        return dummy.next;
+        // return dummy.next;
 
         // ================================
-        // Approach 2: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // Approach 2: Sequential Merging
+        // - Time  Complexity: O(N * k)
+        //   (K is the number of lists.)
+        // - Space Complexity: O(1)
+        //   (Constant space used for pointers)
+        // - Pros: Memory efficient.
+        // - Cons: Slower (O(N * k)). Frequent pointer chasing causes cache misses.
         // ================================
         // if (lists.empty()) return nullptr;
 
@@ -54,6 +54,24 @@ public:
         //     res = mergeTwoLists(res, l);
         // }
         // return res;
+
+        // ================================
+        // Approach 3: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        int amount = lists.size();
+        int interval = 1;
+        while (interval < amount) {
+            for (int i = 0; i < amount - interval; i += interval * 2)
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            interval *= 2;
+        }
+        return amount > 0 ? lists[0] : NULL;
     }
 
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
