356e76897aeb517ffd9b9b281fd5ddca4319ad94
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-15 23:31:01 -0800

    Time: 22 ms (62.14%), Space: 23.3 MB (89.83%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,6 +11,15 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // ================================
+        // Approach 1: Iterative BST traversal
+        // - Time  Complexity: O(h)
+        //   (h is the height of the tree.)
+        // - Space Complexity: O(1)
+        //   (Constant space used for pointers)
+        // - Pros: Memory efficient.
+        // - Cons: Only works for BST.
+        // ================================
         if (!root || !p || !q) return nullptr;
 
         int pVal = p->val;
@@ -27,5 +36,29 @@ public:
             }
         }
         return nullptr;
+
+
+        // ================================
+        // Approach 2: Recursive
+        // - Time  Complexity: O(N)
+        //   (Same as iterative, visits nodes along the path to LCA)
+        // - Space Complexity: O(h)
+        //   (Implicit stack space used by recursion, proportional to tree height)
+        // - Pros: Concise
+        // - Cons: Recursion overhead
+        // ================================
+        // if (!root || !p || !q) return nullptr;
+
+        // int val = root->val;
+        // int pVal = p->val;
+        // int qVal = q->val;
+
+        // if (pVal > val && qVal > val) {
+        //     return lowestCommonAncestor(root->right, p, q);
+        // } else if (pVal < val && qVal < val) {
+        //     return lowestCommonAncestor(root->left, p, q);
+        // } else {
+        //     return root;
+        // }
     }
 };
