548ea12f8e98c8a98122f350825010a9c777ff09
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-17 20:25:04 -0800

    Time: 0 ms (100%), Space: 21.7 MB (94.17%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -13,33 +13,47 @@ class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // Approach 1: Recursive DFS
+        // - Time  Complexity: O(N)
+        //   (Visits each node once)
+        // - Space Complexity: O(h)
+        //   (h is the height of tree)
+        // - Pros: Intuitive.
+        // - Cons: Recursion overhead
         // ================================
-        if (root == nullptr) return true;
-        return dfs(root, LONG_MIN, LONG_MAX);
+        // if (root == nullptr) return true;
+        // return dfs(root, LONG_MIN, LONG_MAX);
 
         // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
+        // Approach 2: In-order Traversal
+        // - Time  Complexity: O(N)
+        //   (Visits each node once)
+        // - Space Complexity: O(h)
+        //   (h is the height of tree)
         // - Pros: 
-        // - Cons: 
+        // - Cons: Need to track the previous node
         // ================================
+        return inorder(root);
     }
 
+    // Approach 1
     bool dfs(TreeNode* node, long long minVal, long long maxVal) {
         if (node == nullptr) return true;
 
         if (node->val <= minVal || node->val >= maxVal) return false;
 
         return dfs(node->left, minVal, node->val) && dfs(node->right, node->val, maxVal);
+    }
+
+    // Approach 2
+    TreeNode* prev = nullptr;
+    bool inorder(TreeNode* node) {
+        if (node == nullptr) return true;
+
+        if (!inorder(node->left)) return false;
+        if (prev != nullptr && node->val <= prev->val) return false;
+
+        prev = node;
+        return inorder(node->right);
     }
 };
