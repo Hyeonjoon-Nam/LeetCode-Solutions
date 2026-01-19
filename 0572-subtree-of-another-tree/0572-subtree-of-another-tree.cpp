066112062735a723b8b9c730fb11574a2f9efdd3
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-19 14:19:52 -0800

    Time: 15 ms (5.08%), Space: 37.6 MB (5.16%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -13,30 +13,37 @@ class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // Approach 1: Double recursive DFS
+        // - Time  Complexity: O(M * N)
+        //   (M is the number of nodes in root, N is the number of nodes in subRoot)
+        // - Space Complexity: O(H)
+        //   (H is the height of the root tree due to recursino stack.)
+        // - Pros: Highly modular.
+        // - Cons: Inefficient if many nodes have the same value.
         // ================================
-        if (subRoot == nullptr) return true;
-        if (root == nullptr) return false;
+        // if (subRoot == nullptr) return true;
+        // if (root == nullptr) return false;
 
-        if (isSameTree(root, subRoot)) return true;
+        // if (isSameTree(root, subRoot)) return true;
 
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        // return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
 
         // ================================
-        // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // Approach 2: String serialization
+        // - Time  Complexity: O(M + N)
+        //   (Serialization takes O(M + N), and string search is very efficient.)
+        // - Space Complexity: O(M + N)
+        //   (Stores the serialized strings of both trees)
+        // - Pros: Faster than the first approach.
+        // - Cons: Requires carefull delimiter/null handling.
         // ================================
+        if (!subRoot) return true;
+        if (!root) return false;
+
+        string s1 = Serialize(root);
+        string s2 = Serialize(subRoot);
+        
+        return s1.find(s2) != string::npos;
     }
 
     // For approach 1
@@ -45,5 +52,12 @@ public:
         if (!p || !q || p->val != q->val) return false;
 
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); 
+    }
+
+    // For approach 2
+    string Serialize(TreeNode* root) {
+        if (!root) return ",#";
+        
+        return "," + to_string(root->val) + Serialize(root->left) + Serialize(root->right);
     }
 };
