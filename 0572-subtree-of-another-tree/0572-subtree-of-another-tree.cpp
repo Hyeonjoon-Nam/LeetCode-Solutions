4cf8eacdc3f7907a50940d28700325bba4c9f214
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-19 14:19:48 -0800

    Time: 0 ms (100%), Space: 29.1 MB (40.04%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        if (subRoot == nullptr) return true;
+        if (root == nullptr) return false;
+
+        if (isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+    }
+
+    // For approach 1
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q || p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); 
+    }
+};
