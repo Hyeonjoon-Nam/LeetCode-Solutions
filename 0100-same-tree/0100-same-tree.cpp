24ac3900fadf177f34c879c638289b0679638dc8
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-19 13:38:35 -0800

    Time: 0 ms (100%), Space: 12.8 MB (76.42%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // ================================
+        // Approach 1: Recursive DFS
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        if (p == nullptr && q == nullptr) return true;
+        else if (p == nullptr) return false;
+        else if (q == nullptr) return false;
+        else if (p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+    }
+};
