bd8ae3420d82ca516f5684fcccc7214374963b49
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-21 08:37:07 -0800

    Time: 7 ms (94.75%), Space: 75.8 MB (5.33%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,14 +1,63 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> q;
+        // ================================
+        // Approach 1: Min-Heap (K-sized)
+        // - Time  Complexity: O(N log k)
+        //   (N times of push/pop computation, each computation takes O(log k))
+        // - Space Complexity: O(k)
+        //   (Only stores k number of elements)
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // priority_queue<int, vector<int>, greater<int>> q;
 
-        for (int x : nums) {
-            q.push(x);
+        // for (int x : nums) {
+        //     q.push(x);
 
-            if (q.size() > k) q.pop();
+        //     if (q.size() > k) q.pop();
+        // }
+
+        // return q.top();
+
+        // ================================
+        // Approach 2: Quickselect
+        // - Time  Complexity: Average O(N), Worst O(N²)
+        //   (Average O(N) due to random pivot selection, O(N²) if pivot is consistently the min/max)
+        // - Space Complexity: O(N)
+        //   (New vector left, mid, right are created at each steps)
+        // - Pros: Faster on average than Heap approach.
+        // - Cons: Performance is highly dependent on pivot choice, O(N) extra space
+        // ================================
+        return quickSelect(nums, k);
+    }
+    
+    // For approach 2
+    int quickSelect(vector<int>& nums, int k) {
+        int pivot = nums[rand() % nums.size()];
+
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
+
+        for (int num : nums) {
+            if (num > pivot) {
+                left.push_back(num);
+            } else if (num < pivot) {
+                right.push_back(num);
+            } else {
+                mid.push_back(num);
+            }
         }
 
-        return q.top();
+        if (k <= left.size()) {
+            return quickSelect(left, k);
+        }
+
+        if (left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+
+        return pivot;
     }
 };
