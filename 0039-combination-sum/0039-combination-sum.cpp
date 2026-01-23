a2043a7117193e284fe97252090c78c6127da813
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-23 08:37:25 -0800

    Time: 1 ms (64.44%), Space: 14 MB (75.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // ================================
+        // Approach 1: Backtracking with Element Reuse & Pruning
+        // - Time  Complexity: O(N ^ ((T/M) + 1))
+        //   (N: number of candidates, T: target value, M: minimal value in candidates)
+        // - Space Complexity: O(T/M)
+        //   (The maximum depth of the recursion stack is target divided by the smallest candidate.)
+        // - Pros: Efficiently finds all unique combinations, sorting allows for early pruning.
+        // - Cons: Can be slow if the target is large ans candidates are very small numbers.
+        // ================================
         vector<vector<int>> ans;
         vector<int> path;
 
@@ -10,6 +19,7 @@ public:
         return ans;
     }
 
+    // For approach 1
     void backtrack(int start, vector<int>& candidates, vector<int>& path, vector<vector<int>>& ans, int target) {
         if (target == 0) {
             ans.push_back(path);
