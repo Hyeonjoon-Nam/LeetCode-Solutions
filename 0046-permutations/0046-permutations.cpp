403949afeb82f9c320b8eca2f8374d55f130a721
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-24 11:14:13 -0800

    Time: 0 ms (100%), Space: 10.4 MB (87.37%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> path;
-        vector<bool> used(nums.size(), false);
+        // ================================
+        // Approach 1: Backtracking with 'used' array
+        // - Time  Complexity: O(N * N!)
+        //   (There are N! permutations, and copying each to 'res' takes O(N) time.)
+        // - Space Complexity: O(N)
+        //   (Recursion depth is N, 'path' and 'used' arrays take O(N) space.)
+        // - Pros: Intuitive.
+        // - Cons: Requires extra space for the 'used' array.
+        // ================================
+        // vector<int> path;
+        // vector<bool> used(nums.size(), false);
+        // vector<vector<int>> res;
+        
+        // backtrack(nums, path, used, res);
+
+        // return res;
+
+        // ================================
+        // Approach 2: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
         vector<vector<int>> res;
         
-        backtrack(nums, path, used, res);
+        backtrack_swap(0, nums, res);
 
         return res;
     }
 
+    // For approach 1
     void backtrack(vector<int>& nums, vector<int>& path, vector<bool>& used, vector<vector<int>>& res) {
         if (path.size() == nums.size()) {
             res.push_back(path);
@@ -26,6 +51,20 @@ public:
 
             path.pop_back();
             used[i] = false;
+        }
+    }
+
+    // For approach 2
+    void backtrack_swap(int start, vector<int>& nums, vector<vector<int>>& res) {
+        if (start == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[i], nums[start]);
+            backtrack_swap(start + 1, nums, res);
+            swap(nums[i], nums[start]);
         }
     }
 };
