702b1bbc1935f75e7696c0dff70cc205d4bd19e4
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-26 15:57:03 -0800

    Time: 0 ms (100%), Space: 10.3 MB (96.5%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        // ================================
+        // Approach 1: Backtracking with Pruning
+        // - Time  Complexity: O(N * 2^N)
+        //   (Sorting takes O(N log N), and there are at most 2^N subsets)
+        // - Space Complexity: O(N)
+        //   (The depth of the recursion stack is N.)
+        // - Pros: Efficiently hadnles duplicate elements without post-processing.
+        // - Cons: Requires the input array to be sorted first.
+        // ================================
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> res;
@@ -10,6 +19,7 @@ public:
         return res;
     }
 
+    // For approach 1
     void backtrack(int start, vector<int>& path, vector<int>& nums, vector<vector<int>>& res) {
         res.push_back(path);
 
