67a992350def7db1d1d0555dea8681fc281bbba8
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-28 09:34:07 -0800

    Time: 0 ms (100%), Space: 9.4 MB (38.73%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -10,12 +10,12 @@ public:
     vector<string> letterCombinations(string digits) {
         // ================================
         // Approach 1: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // - Time  Complexity: O(4^N * N)
+        //   (N is the number of digits.)
+        // - Space Complexity: O(N)
+        //   (The recursion stack is equal to the length of digits N.)
+        // - Pros: Concise.
+        // - Cons: Recursive overhead.
         // ================================
         if (digits.empty()) return {};
 
@@ -27,7 +27,7 @@ public:
     }
 
     // For approach 1
-    void backtrack(int index, string& digits, string& path, vector<string>& res) {
+    void backtrack(int index, string digits, string path, vector<string>& res) {
         if (index == digits.length()) {
             res.push_back(path);
             return;
