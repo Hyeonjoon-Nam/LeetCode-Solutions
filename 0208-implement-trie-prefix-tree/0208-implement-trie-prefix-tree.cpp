8b60f132c27797605fe88b576afde85001246ed0
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-29 08:48:34 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,39 @@
+class Trie {
+private:
+    unordered_set<string> s;
+
+public:
+    Trie() {
+            s.clear();
+    }
+    
+    void insert(string word) {
+        s.insert(word);
+    }
+    
+    bool search(string word) {
+        return s.find(word) != s.end();
+    }
+    
+    bool startsWith(string prefix) {
+        for (auto& str : s) {
+            if (str.length() < prefix.length()) continue;
+
+            int p = 0;
+            while (p < prefix.length()) {
+                if (str[p] != prefix[p]) continue;
+                p++;
+            }
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
