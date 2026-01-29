1f0b0e900c077de7d830b708cda490dd9151bcdd
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-29 09:12:52 -0800

    Time: 17 ms (75.64%), Space: 50.5 MB (53.15%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,32 +1,107 @@
+struct TrieNode {
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
 class Trie {
 private:
+    // Approach 1
     unordered_set<string> s;
 
+    // Approach 2
+    TrieNode* root;
+
 public:
+    // ================================
+    // Approach 1: 
+    // - Time  Complexity: O()
+    //   ()
+    // - Space Complexity: O()
+    //   ()
+    // - Pros: 
+    // - Cons: 
+    // ================================
+    
+    // ================================
+    // Approach 2: 
+    // - Time  Complexity: O()
+    //   ()
+    // - Space Complexity: O()
+    //   ()
+    // - Pros: 
+    // - Cons: 
+    // ================================
     Trie() {
-            s.clear();
+        // Approach 1
+        // s.clear();
+        
+        // Approach 2
+        root = new TrieNode();
     }
     
     void insert(string word) {
-        s.insert(word);
+        // Approach 1
+        // s.insert(word);
+        
+        // Approach 2
+        TrieNode* curr = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) {
+                curr->children[idx] = new TrieNode();
+            }
+            curr = curr->children[idx];
+        }
+        curr->isEndOfWord = true;
     }
     
     bool search(string word) {
-        return s.find(word) != s.end();
+        // Approach 1
+        // return s.find(word) != s.end();
+        
+        // Approach 2
+        TrieNode* curr = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) {
+                return false;
+            }
+            curr = curr->children[idx];
+        }
+        return curr->isEndOfWord;
     }
     
     bool startsWith(string prefix) {
-        for (auto& str : s) {
-            if (str.length() < prefix.length()) continue;
+        // Approach 1
+        // for (auto& str : s) {
+        //     if (str.length() < prefix.length()) continue;
 
-            int p = 0;
-            while (p < prefix.length()) {
-                if (str[p] != prefix[p]) break;
-                p++;
+        //     int p = 0;
+        //     while (p < prefix.length()) {
+        //         if (str[p] != prefix[p]) break;
+        //         p++;
+        //     }
+        //     if (p == prefix.length()) return true;
+        // }
+        // return false;
+        
+        // Approach 2
+        TrieNode* curr = root;
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) {
+                return false;
             }
-            if (p == prefix.length()) return true;
+            curr = curr->children[idx];
         }
-        return false;
+        return true;
     }
 };
 
