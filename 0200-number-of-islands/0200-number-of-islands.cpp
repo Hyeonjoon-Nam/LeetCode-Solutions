4afb93a6a9aa3a7ca10ed8f9a8cf3ce77277df0c
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-01-30 20:59:37 -0800

    Time: 21 ms (91.26%), Space: 16.2 MB (78.8%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        int count = 0;
+
+        for (int r = 0; r < grid.size(); r++) {
+            for (int c = 0; c < grid[0].size(); c++) {
+                if (grid[r][c] == '1') {
+                    count++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == '0')
+            return;
+        
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+};
