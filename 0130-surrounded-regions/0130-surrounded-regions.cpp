fe5a434d1ddbd7a3a93c1d2bd68918a006dadf61
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-03 09:05:15 -0800

    Time: 0 ms (100%), Space: 13.8 MB (89.78%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
+        // ================================
+        // Approach 1: DFS
+        // - Time  Complexity: O(M * N)
+        //   (M is the rows, N is the columns. Visits each node at most once.)
+        // - Space Complexity: O(M * N)
+        //   (In the worst case, recursion stack depth is M * N.)
+        // - Pros: Optimal, meets the requirement in-place replacing.
+        // - Cons: Potential for Stack Overflow on very large grids.
+        // ================================
+        if (board.empty()) return;
+
         int m = board.size();
         int n = board[0].size();
 
@@ -23,12 +34,11 @@ public:
         }
     }
 
+    // For approach 1
     void backtrack(vector<vector<char>>& board, int r, int c) {
-        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] == 'T' || board[r][c] == 'X') return;
+        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != 'O') return;
         
-        if (board[r][c] == 'O') {
-            board[r][c] = 'T';
-        }
+        board[r][c] = 'T';
 
         backtrack(board, r+1, c);
         backtrack(board, r-1, c);
