9cbfaeb19d5364b197d0d49ce73843642e889731
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-04 08:02:24 -0800

    Time: 7 ms (48.21%), Space: 19.1 MB (91.41%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> adj(numCourses);
+        for (auto& pre : prerequisites) {
+            adj[pre[1]].push_back(pre[0]);
+        }
+
+        vector<int> state(numCourses, 0);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (hasCycle(i, adj, state)) return false;
+            }
+        }
+        return true;
+    }
+
+    bool hasCycle(int curr, vector<vector<int>>& adj, vector<int>& state) {
+        if (state[curr] == 1) return true;
+        if (state[curr] == 2) return false;
+
+        state[curr] = 1;
+        for (int next : adj[curr]) {
+            if (hasCycle(next, adj, state)) return true;
+        }
+        state[curr] = 2;
+        return false;
+    }
+};
