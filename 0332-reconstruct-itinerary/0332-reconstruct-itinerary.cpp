b4790e6537dc40feb2ba48892bca7f4eab5c0c02
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-07 22:47:22 -0800

    Time: 15 ms (9.77%), Space: 21.1 MB (41.25%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // For approach 1
+    unordered_map<string, multiset<string>> adj;
+    vector<string> res;
+
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        // ================================
+        // Approach 1: Greedy DFS
+        // - Time  Complexity: O(E log E)
+        //   (E is the number of tickets, sorting neighbor)
+        // - Space Complexity: O(V + E)
+        //   (Storing the graph takes O(V+E) and the recursion stack can go up to O(E))
+        // - Pros: Guarantees finding the lexical smallest Eulerian path efficiently.
+        // - Cons: Risk of stack overflow.
+        // ================================
+        for (auto& t : tickets) {
+            adj[t[0]].insert(t[1]);
+        }
+
+        dfs("JFK");
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    // For approach 1
+    void dfs(string u) {
+        while (!adj[u].empty()) {
+            string v = *adj[u].begin();
+            adj[u].erase(adj[u].begin());
+            dfs(v);
+        }
+        res.push_back(u);
+    }
+};
