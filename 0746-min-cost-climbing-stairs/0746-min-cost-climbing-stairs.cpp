b5468867f109a07d9d1d99e7f553317161e8b5bf
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-11 08:10:39 -0800

    Time: 0 ms (100%), Space: 17.4 MB (89.18%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -10,18 +10,18 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
-        // if (cost.empty()) return 0;
+        if (cost.empty()) return 0;
 
-        // int n = cost.size();
+        int n = cost.size();
 
-        // int dp[n + 2];
-        // dp[n] = 0;
-        // dp[n+1] = 0;
+        int dp[n + 2];
+        dp[n] = 0;
+        dp[n+1] = 0;
 
-        // for (int i = n - 1; i >= 0; i--) {
-        //     dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
-        // }
-        // return min(dp[0], dp[1]);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+        }
+        return min(dp[0], dp[1]);
         
         // ================================
         // Approach 2: 
@@ -32,20 +32,20 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
-        if (cost.empty()) return 0;
+        // if (cost.empty()) return 0;
 
-        int n = cost.size();
+        // int n = cost.size();
 
-        int next1 = 0;
-        int next2 = 0;
+        // int next1 = 0;
+        // int next2 = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            int current = cost[i] + min(next1, next2);
+        // for (int i = n - 1; i >= 0; i--) {
+        //     int current = cost[i] + min(next1, next2);
 
-            next2 = next1;
-            next1 = current;
-        }
+        //     next2 = next1;
+        //     next1 = current;
+        // }
 
-        return min(next1, next2);
+        // return min(next1, next2);
     }
 };
