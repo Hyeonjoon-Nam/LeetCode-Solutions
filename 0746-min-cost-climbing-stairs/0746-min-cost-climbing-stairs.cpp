659086c9230e762388fc0543483bfdf92b6837b9
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-11 08:08:00 -0800

    Time: 0 ms (100%), Space: 17.5 MB (89.18%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // if (cost.empty()) return 0;
+
+        // int n = cost.size();
+
+        // int dp[n + 2];
+        // dp[n] = 0;
+        // dp[n+1] = 0;
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+        // }
+        // return min(dp[0], dp[1]);
+        
+        // ================================
+        // Approach 2: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
         if (cost.empty()) return 0;
 
         int n = cost.size();
 
-        int dp[n + 2];
-        dp[n] = 0;
-        dp[n+1] = 0;
+        int next1 = 0;
+        int next2 = 0;
 
         for (int i = n - 1; i >= 0; i--) {
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2]);
+            int current = cost[i] + min(next1, next2);
+
+            next2 = next1;
+            next1 = current;
         }
-        return min(dp[0], dp[1]);
+
+        return min(next1, next2);
     }
 };
