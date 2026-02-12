6c43aa9e6c8ce0f90a0c863437c805c97067c273
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-12 08:31:14 -0800

    Time: 7 ms (86.43%), Space: 9.5 MB (55.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -12,45 +12,29 @@ public:
         // ================================
         if (s.length() <= 1) return s;
 
-        int center = 0, left = 0, right = 0;
-        int maxLength = 0;
+        int start = 0;
+        int maxLen = 0;
         string res;
+
         for (int i = 0; i < s.length(); i++) {
-            string odd = oddPalindrome(i, s);
-            string even = evenPalindrome(i, i+1, s);
+            int len1 = Palindrome(s, i, i);
+            int len2 = Palindrome(s, i, i + 1);
 
-            if (maxLength < odd.length()) {
-                maxLength = odd.length();
-                res = odd;
-            }
-            if (maxLength < even.length()) {
-                maxLength = even.length();
-                res = even;
+            int currentMax = max(len1, len2);
+
+            if (currentMax > maxLen) {
+                maxLen = currentMax;
+                start = i - (currentMax - 1) / 2;
             }
         }
-        return res;
+        return s.substr(start, maxLen);
     }
 
-    string oddPalindrome(int center, const string& s) {
-        int left = center - 1;
-        int right = center + 1;
-
+    int Palindrome(const string& s, int left, int right) {
         while (left >= 0 && right < s.length() && s[left] == s[right]) {
             left--;
             right++;
         }
-        left++;
-        right--;
-        return s.substr(left, right - left + 1);
-    }
-    
-    string evenPalindrome(int left, int right, const string& s) {
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        left++;
-        right--;
-        return s.substr(left, right - left + 1);
+        return right - left - 1;
     }
 };
