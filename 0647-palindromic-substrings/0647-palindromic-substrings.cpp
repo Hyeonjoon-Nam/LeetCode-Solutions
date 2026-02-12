9f0103071b40d198dfcae181fbbfde328b14ed5f
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-12 08:40:37 -0800

    Time: 5 ms (65.63%), Space: 8.7 MB (63.33%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        // ================================
+        // Approach 1: Expand from center
+        // - Time  Complexity: O(N²)
+        //   (N centers, and each expansion takes up to O(N).)
+        // - Space Complexity: O(1)
+        //   (Only constant space used.)
+        // - Pros: Optimal space.
+        // - Cons: Still O(N²) time.
+        // ================================
         int n = s.length();
         int count = 0;
 
@@ -12,6 +21,7 @@ public:
         return count;
     }
 
+    // For approach 1
     void expand(const string& s, int left, int right, int& count) {
         while (left >= 0 && right < s.length() && s[left] == s[right]) {
             left--;
