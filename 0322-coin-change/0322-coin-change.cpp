0ab9976ab0ac99d4752e8ca3178ffe30ac6bedba
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-13 09:14:58 -0800

    Time: 15 ms (92.12%), Space: 18 MB (71.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        // ================================
+        // Approach 1: Dynamic Programming
+        // - Time  Complexity: O(N * M)
+        //   (N is amount, M is the number of coin denominations.)
+        // - Space Complexity: O(N)
+        //   (An array of size amount + 1 is used to store minimum coins.)
+        // - Pros: Optimal time complexity.
+        // - Cons: Requires O(N) extra space.
+        // ================================
         if (amount == 0) return 0;
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
