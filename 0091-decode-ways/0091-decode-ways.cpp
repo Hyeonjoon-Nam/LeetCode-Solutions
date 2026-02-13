cdc191a7d74695b43d5a8899da4b8e68f1e22643
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-13 08:35:11 -0800

    Time: 0 ms (100%), Space: 8.4 MB (89.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -10,25 +10,60 @@ public:
         // - Pros: 
         // - Cons: 
         // ================================
+        // if (s.empty() || s[0] == '0') return 0;
+
+        // int n = s.length();
+        // vector<int> dp(n + 1, 0);
+
+        // dp[0] = 1;
+        // dp[1] = 1;
+
+        // for (int i = 2; i <= n; i++) {
+        //     int oneDigit = s[i - 1] - '0';
+        //     if (oneDigit >= 1 && oneDigit <= 9) {
+        //         dp[i] += dp[i - 1];
+        //     }
+
+        //     int twoDigit = (s[i - 2] - '0') * 10 + oneDigit;
+        //     if (twoDigit >= 10 && twoDigit <= 26) {
+        //         dp[i] += dp[i - 2];
+        //     }
+        // }
+        // return dp[n];
+
+
+        // ================================
+        // Approach 2: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
         if (s.empty() || s[0] == '0') return 0;
 
         int n = s.length();
-        vector<int> dp(n + 1, 0);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev1 = 1;
+        int prev2 = 1;
+        int current = 1;
 
         for (int i = 2; i <= n; i++) {
+            current = 0;
             int oneDigit = s[i - 1] - '0';
             if (oneDigit >= 1 && oneDigit <= 9) {
-                dp[i] += dp[i - 1];
+                current += prev1;
             }
 
             int twoDigit = (s[i - 2] - '0') * 10 + oneDigit;
             if (twoDigit >= 10 && twoDigit <= 26) {
-                dp[i] += dp[i - 2];
+                current += prev2;
             }
+
+            prev2 = prev1;
+            prev1 = current;
         }
-        return dp[n];
+        return current;
     }
 };
