c64e281bd0c5ab6584e7bbd3cb3b60eeb3e45209
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-17 15:45:50 -0800

    Time: 11 ms (98.23%), Space: 9.1 MB (93.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,7 +2,34 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         // ================================
-        // Approach 1: 
+        // Approach 1: Dynamic Programming (2D Array)
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // int m = text1.length();
+        // int n = text2.length();
+
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // for (int i = 1; i <= m; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (text1[i - 1] == text2[j - 1]) {
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         }
+        //         else {
+        //             dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        //         }
+        //     }
+        // }
+
+        // return dp[m][n];
+
+        // ================================
+        // Approach 2: Dynamic Programming (Optimized 1D Array)
         // - Time  Complexity: O()
         //   ()
         // - Space Complexity: O()
@@ -13,29 +40,25 @@ public:
         int m = text1.length();
         int n = text2.length();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        if (m < n) return longestCommonSubsequence(text2, text1);
+
+        vector<int> dp(n + 1, 0);
 
         for (int i = 1; i <= m; i++) {
+            int prev = 0;
             for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+
                 if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[j] = 1 + prev;
                 }
                 else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[j] = max(dp[j], dp[j - 1]);
                 }
+                prev = temp;
             }
         }
 
-        return dp[m][n];
-
-        // ================================
-        // Approach 2: 
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
-        // ================================
+        return dp[n];
     }
 };
