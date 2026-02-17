f99069291c9be0676f0c0bcda45100c4a19fabb8
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-17 15:18:32 -0800

    Time: 0 ms (100%), Space: 8.4 MB (75.9%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,50 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
+        // ================================
+        // Approach 1: Dynamic Programming (2D Array)
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // vector<vector<int>> dp(m, vector<int>(n, 1));
+
+        // for (int i = 1; i < m; i++) {
+        //     for (int j = 1; j < n; j++) {
+        //         dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        // ================================
+        // Approach 2: Dynamic Programming (Optimized 1D Array)
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        vector<int> dp(n, 1);
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[j] += dp[j - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[n - 1];
+
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
     }
 };
