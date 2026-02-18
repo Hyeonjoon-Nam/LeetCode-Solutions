1335d5c54aec18e77b152bbfe126ba8b9cc30b71
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-18 08:57:18 -0800

    Time: 4 ms (93.2%), Space: 10 MB (96.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,12 +3,12 @@ public:
     int change(int amount, vector<int>& coins) {
         // ================================
         // Approach 1: Dynamic Programming
-        // - Time  Complexity: O()
-        //   ()
-        // - Space Complexity: O()
-        //   ()
-        // - Pros: 
-        // - Cons: 
+        // - Time  Complexity: O(n * amount)
+        //   (n is the number of coin denominations.)
+        // - Space Complexity: O(amount)
+        //   (The 'dp' array takes O(amont) space.)
+        // - Pros: Space efficient.
+        // - Cons: Requires unsigned int to handle signed integer overflow.
         // ================================
         vector<unsigned int> dp(amount + 1, 0);
         dp[0] = 1;
