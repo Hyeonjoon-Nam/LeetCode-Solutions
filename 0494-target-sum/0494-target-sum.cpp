a502d01e4d2ae7674a2aa537b213cc6d7bbf17f9
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-19 22:56:55 -0800

    Time: 0 ms (100%), Space: 12.4 MB (68.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        // ================================
+        // Approach 1: Dynamic Programming
+        // - Time  Complexity: O(N * P)
+        //   (N: nums.size(), P: (totalSum + target) / 2. We iterate the grid once.)
+        // - Space Complexity: O(P)
+        //   (1D DP array stores ways to achieve each subset sum.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
         int n = nums.size();
         int totalSum = 0;
         for (int num : nums) totalSum += num;
