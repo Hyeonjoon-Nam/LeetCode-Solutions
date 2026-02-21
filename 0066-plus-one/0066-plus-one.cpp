b40b6dd7b03494cb19e76b8e97862b4193a483a2
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-20 23:20:14 -0800

    Time: 0 ms (100%), Space: 11.7 MB (18.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,23 +1,56 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> res;
-        int n = digits.size();
-        int add = 1;
+        // ================================
+        // Approach 1: Reconstruct with reverse.
+        // - Time  Complexity: O(n)
+        //   (Single pass plus a reverse operation.)
+        // - Space Complexity: O(n)
+        //   (Creates a new vector to store results.)
+        // - Pros: Simple.
+        // - Cons: Uses extra memory.
+        // ================================
+        // vector<int> res;
+        // int n = digits.size();
+        // int add = 1;
 
-        for (int i = n - 1; i >= 0; i--) {
-            int sum = digits[i] + add;
-            if (sum == 10) {
-                add = 1;
-                res.push_back(0);
+        // for (int i = n - 1; i >= 0; i--) {
+        //     int sum = digits[i] + add;
+        //     if (sum == 10) {
+        //         add = 1;
+        //         res.push_back(0);
+        //     }
+        //     else {
+        //         add = 0;
+        //         res.push_back(sum);
+        //     }
+        // }
+        // if (add) res.push_back(1);
+        // reverse(res.begin(), res.end());
+        // return res;
+
+        
+        // ================================
+        // Approach 2: In-place
+        // - Time  Complexity: O(n)
+        //   (Average O(1) if the last digit is not 9, worst O(n).)
+        // - Space Complexity: O(1)
+        //   (Constant space used.)
+        // - Pros: Memory efficient.
+        // - Cons: Modifies the original input vector.
+        // ================================
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
             }
             else {
-                add = 0;
-                res.push_back(sum);
+                digits[i]++;
+                return digits;
             }
         }
-        if (add) res.push_back(1);
-        reverse(res.begin(), res.end());
-        return res;
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
