f85e17749f5c243871a91dbd1983bb4cff218661
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-20 23:31:43 -0800

    Time: 0 ms (100%), Space: 10.2 MB (69.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // ================================
+        // Approach 1: Transpse & Reverse
+        // - Time  Complexity: O(N²)
+        //   (We visit each element twice, once for transpose, once for reverse.)
+        // - Space Complexity: O(1)
+        //   (Rotation is performed in-place.)
+        // - Pros: Neat.
+        // - Cons: 
+        // ================================
         transpose(matrix);
         reverseRow(matrix);
     }
 
+    // For approach 1
     void transpose(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
@@ -15,6 +25,7 @@ public:
         }
     }
 
+    // For approach 1
     void reverseRow(vector<vector<int>>& matrix) {
         for (vector<int>& v : matrix) {
             reverse(v.begin(), v.end());
