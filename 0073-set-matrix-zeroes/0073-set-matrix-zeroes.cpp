f19c1288d24edcae2f0943d19e6dfa1cc9a4b0e5
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-21 22:18:35 -0800

    Time: 0 ms (100%), Space: 18.5 MB (95.47%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // ================================
+        // Approach 1: In-place
+        // - Time  Complexity: O(M * N)
+        //   (Two passes through the matrix.)
+        // - Space Complexity: O(1)
+        //   (Constant space used.)
+        // - Pros: Optimal space complexity.
+        // - Cons: 
+        // ================================
+        bool isCol = false;
+
+        int r = matrix.size();
+        int c = matrix[0].size();
+
+        for (int i = 0; i < r; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+            for (int j = 1; j < c; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < c; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (isCol) {
+            for (int i = 0; i < r; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
