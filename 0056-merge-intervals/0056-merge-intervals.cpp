fab1cd0d5a0457db2d978f574b01d8b99f6f9601
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-24 13:36:17 -0800

    Time: 3 ms (89.25%), Space: 24 MB (31.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
+        // sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+        //     return a[0] < b[0];
+        // });
+
+        // vector<vector<int>> res;
+
+        // for (vector<int>& v : intervals) {
+        //     if (!res.empty()) {
+        //         if (res.back()[1] >= v[0]) {
+        //             int start = min(res.back()[0], v[0]);
+        //             int end = max(res.back()[1], v[1]);
+
+        //             res.pop_back();
+        //             res.push_back({start, end});
+        //         }
+        //         else
+        //             res.push_back(v);
+        //     }
+        //     else
+        //         res.push_back(v);
+        // }
+
+        // return res;
+
+        // ================================
+        // Approach 1: 
+        // - Time  Complexity: O()
+        //   ()
+        // - Space Complexity: O()
+        //   ()
+        // - Pros: 
+        // - Cons: 
+        // ================================
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
             return a[0] < b[0];
         });
@@ -10,11 +52,8 @@ public:
         for (vector<int>& v : intervals) {
             if (!res.empty()) {
                 if (res.back()[1] >= v[0]) {
-                    int start = min(res.back()[0], v[0]);
                     int end = max(res.back()[1], v[1]);
-
-                    res.pop_back();
-                    res.push_back({start, end});
+                    res.back()[1] = end;
                 }
                 else
                     res.push_back(v);
