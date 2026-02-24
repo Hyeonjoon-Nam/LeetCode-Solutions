d00bc29e830dbce542dea845f94da9bcb94bba85
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-24 13:55:11 -0800

    Time: 0 ms (100%), Space: 21.8 MB (21.74%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,10 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        return merge(intervals);
+        // ================================
+        // Approach 1: Append and Merge
+        // - Time  Complexity: O(N log N)
+        //   (Pushing back takes O(1), but merge() sorts the entire array.)
+        // - Space Complexity: O(N)
+        //   (Allocates a new result vector, plus O(log N) for sorting space.)
+        // - Pros: Simple.
+        // - Cons: Not optimal time complexity.
+        // ================================
+        // intervals.push_back(newInterval);
+        // return merge(intervals);
+
+        // ================================
+        // Approach 2: Linear Traversal
+        // - Time  Complexity: O(N)
+        //   (Single pass through the intervals array.)
+        // - Space Complexity: O(1)
+        //   (Excluding the result vector, only use a few integer pointers.)
+        // - Pros: Optimal.
+        // - Cons: Allocates a new vector.
+        // ================================
+        vector<vector<int>> res;
+        int n = intervals.size();
+        int i = 0;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            ++i;
+        }
+
+        while (i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            ++i;
+        }
+        res.push_back(newInterval);
+
+        while (i < n) {
+            res.push_back(intervals[i]);
+            ++i;
+        }
+
+        return res;
     }
 
+    // For approach 1
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
             return a[0] < b[0];
