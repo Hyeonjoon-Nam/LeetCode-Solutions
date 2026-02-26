f865f238623a3c619a124ff512eee23f1ab4d35e
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-25 21:51:28 -0800

    Time: 4 ms (35.52%), Space: 16.1 MB (81.93%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        // ================================
+        // Approach 1: Sorting + Priority queue
+        // - Time  Complexity: O(N log N)
+        //   (Sorting takes O(N log N), top(), push(), pop() takes O(log N) and we do it N times, so O(N log N))
+        // - Space Complexity: O(N)
+        //   (In the worst case, priority queue can be sized N)
+        // - Pros: Intuitive, optimal.
+        // - Cons: 
+        // ================================
+        int n = intervals.size();
+        if (n <= 1) return n;
+
+        sort(intervals.begin(), intervals.end());
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        minHeap.push(intervals[0][1]);
+
+        for (int i = 1; i < n; i++) {
+            if (minHeap.top() <= intervals[i][0]) {
+                minHeap.pop();
+            }
+            minHeap.push(intervals[i][1]);
+        }
+        return minHeap.size();
+    }
+};
