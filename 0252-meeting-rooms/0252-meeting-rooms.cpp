e84cf77d35564dfbc562fdede842aef155c38024
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-25 20:59:05 -0800

    Time: 0 ms (100%), Space: 16 MB (55.08%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        
+        if (intervals.size() < 2) return true;
         sort(intervals.begin(), intervals.end());
 
         for (int i = 0; i < intervals.size() - 1; i++) {
