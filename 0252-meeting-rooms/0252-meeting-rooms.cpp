6ffa03f02069f7c182d289bb50fe906501901a61
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-25 20:59:11 -0800

    Time: 1 ms (55.28%), Space: 16 MB (55.08%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
+        // ================================
+        // Approach 1: Sorting
+        // - Time  Complexity: O(N log N)
+        //   (Sorting takes O(N log N))
+        // - Space Complexity: O(log N) or O(N)
+        //   (C++ quick sort takes O(log N) on average, but the worst case, O(N))
+        // - Pros: Simple, optimal.
+        // - Cons: Modifies the original intervals array.
+        // ================================
         if (intervals.size() < 2) return true;
         sort(intervals.begin(), intervals.end());
 
