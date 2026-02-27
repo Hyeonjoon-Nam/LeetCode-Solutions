95a60884be54e214a5c64c9dbf789345d9df1bd0
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-02-27 11:14:45 -0800

    Time: 0 ms (100%), Space: 52.3 MB (50.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if (n <= 0) return true;
+        // ================================
+        // Approach 1: Dynamic Programming
+        // - Time  Complexity: O(N²)
+        //   (In the worst case, the inner loop runs up to N times for each element.)
+        // - Space Complexity: O(N)
+        //   (Allocates a boolean vector of size N.)
+        // - Pros: Intuitive.
+        // - Cons: Not optimal.
+        // ================================
+        // int n = nums.size();
+        // if (n <= 0) return true;
 
-        vector<bool> possible(n, false);
-        possible[0] = true;
+        // vector<bool> possible(n, false);
+        // possible[0] = true;
+
+        // for (int i = 0; i < n; ++i) {
+        //     if (!possible[i]) return false;
+
+        //     for (int j = 0; j < nums[i]; ++j) {
+        //         int index = i + j + 1;
+        //         if (index >= n) break;
+        //         else possible[index] = true;
+        //     }
+        // }
+        // return true;
+
+        // ================================
+        // Approach 2: Greedy
+        // - Time  Complexity: O(N)
+        //   (A single linear pass through the array.)
+        // - Space Complexity: O(1)
+        //   (Constant space used.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
+        int n = nums.size();
+        int max_reach = 0;
 
         for (int i = 0; i < n; ++i) {
-            if (!possible[i]) return false;
+            if (i > max_reach) return false;
 
-            for (int j = 0; j < nums[i]; ++j) {
-                int index = i + j + 1;
-                if (index >= n) break;
-                else possible[index] = true;
-            }
+            max_reach = max(max_reach, i + nums[i]);
+
+            if (max_reach >= n - 1) return true;
         }
         return true;
     }
