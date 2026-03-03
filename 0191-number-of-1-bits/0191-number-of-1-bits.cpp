9e446df049701de42ff881048b6a4293a88a3cdf
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-03-02 22:47:10 -0800

    Time: 0 ms (100%), Space: 8.2 MB (81.54%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        // ================================
+        // Approach 1: Bit Manipulation
+        // - Time  Complexity: O(1)
+        //   (Worst case 32 iterations, but practically runs exactly 'k' times where 'k' is the number of 1 bits.)
+        // - Space Complexity: O(1)
+        //   (Only an integer used for the answer.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
         int count = 0;
         while (n) {
             count += n & 0x1;
