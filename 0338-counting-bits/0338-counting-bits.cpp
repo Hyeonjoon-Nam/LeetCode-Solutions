895a37e8b7dc010fee45873d551f00375749ef6e
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-03-02 22:49:43 -0800

    Time: 0 ms (100%), Space: 10.6 MB (80.4%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        // ================================
+        // Approach 1: Bit Manipulation + Dynamic Programming
+        // - Time  Complexity: O(n)
+        //   (A single linear iteration.)
+        // - Space Complexity: O(1)
+        //   (Constant space used except for the required result vector.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
         vector<int> res(n + 1, 0);
 
-        for (int i = 0; i <= n; i++) {
-            res[i] = count(i);
-        }
-        return res;
-    }
-
-    int count(int n) {
-        int res = 0;
-        while (n) {
-            res += n & 0x1;
-            n >>= 1;
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i>>1] + (i&1);
         }
         return res;
     }
