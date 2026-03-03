fd7529909010b96d31306cb42a87f286842faad6
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-03-03 13:32:06 -0800

    Time: 5 ms (19.82%), Space: 8.6 MB (53.96%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
+        // ================================
+        // Approach 1: Math
+        // - Time  Complexity: O(log x)
+        //   (log base 10 of x.)
+        // - Space Complexity: O(1)
+        //   (Constant space used.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
         int res = 0;
         while (x) {
             if (res > INT_MAX / 10 || res < INT_MIN / 10) return 0;
