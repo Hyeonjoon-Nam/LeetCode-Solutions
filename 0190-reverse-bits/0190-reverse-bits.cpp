c5f0a97ae9a895ace8c65c7978f2261cbc1e45c9
HyeonjoonNam <65001204+Hyeonjoon-Nam@users.noreply.github.com>
2026-03-03 13:08:54 -0800

    Time: 0 ms (100%), Space: 8.1 MB (72.95%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int reverseBits(int n) {
+        // ================================
+        // Approach 1: Bit Manipulation
+        // - Time  Complexity: O(1)
+        //   (Runs at most 32 times.)
+        // - Space Complexity: O(1)
+        //   (Constant space used.)
+        // - Pros: Optimal.
+        // - Cons: 
+        // ================================
         int res = 0, pow = 31;
         while (n) {
-            res += (n & 1) << pow;
+            res |= (n & 1) << pow;
             n >>= 1;
             --pow;
         }
